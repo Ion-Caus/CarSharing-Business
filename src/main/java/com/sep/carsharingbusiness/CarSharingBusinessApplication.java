6543a928f860d036e07ed38b868863b9a68505f6
Ion-Caus <causion01@gmail.com>
2021-10-17 13:52:02 +0200

    init

---
+++ src/main/java/com/sep/carsharingbusiness/CarSharingBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.sep.carsharingbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingBusinessApplication.class, args);
+    }
+
+}
