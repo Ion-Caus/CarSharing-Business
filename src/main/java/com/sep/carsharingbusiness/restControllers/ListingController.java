9c877bca0e64f86185230719ece7e5eea1580a58
GeraltWitcher1 <57898274+GeraltWitcher1@users.noreply.github.com>
2021-11-15 17:09:32 +0100

    Added Logging to ListingController and VehicleController

--- src/main/java/com/sep/carsharingbusiness/restControllers/ListingController.java
+++ src/main/java/com/sep/carsharingbusiness/restControllers/ListingController.java
@@ -2,6 +2,7 @@ package com.sep.carsharingbusiness.restControllers;
 
 import com.google.gson.Gson;
 import com.sep.carsharingbusiness.graphQLServices.IListingService;
+import com.sep.carsharingbusiness.log.Log;
 import com.sep.carsharingbusiness.model.Listing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -31,10 +32,13 @@ public class ListingController {
             @RequestParam(value = "dateTo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTo
     ) {
         try {
-            return gson.toJson( listingService.getListing(location, dateFrom, dateTo) );
+            String result = gson.toJson(listingService.getListing(location, dateFrom, dateTo));
+            Log.addLog("|restControllers/ListingController.getListing| : Request : " + location + " "+ dateFrom + " " + dateTo);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/ListingController.getListing| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -43,10 +47,13 @@ public class ListingController {
     public synchronized String addListing(@RequestBody String json) {
         try {
             Listing listing = gson.fromJson(json, Listing.class);
-            return gson.toJson( listingService.addListing(listing) );
+            String result = gson.toJson(listingService.addListing(listing));
+            Log.addLog("|restControllers/ListingController.addListing| : Request : " + json);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/ListingController.addListing| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -57,10 +64,13 @@ public class ListingController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The id from param does not match with the listing's id.");
         }
         try {
-            return gson.toJson( listingService.updateListing(listing) );
+            String result = gson.toJson(listingService.updateListing(listing));
+            Log.addLog("|restControllers/ListingController.updateListing| : Reply : " + result);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/ListingController.updateListing| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -69,10 +79,12 @@ public class ListingController {
     public synchronized HttpStatus removeListing(@RequestParam(value = "id") int id) {
         try {
             listingService.removeListing(id);
+            Log.addLog("|restControllers/ListingController.removeListing| : Request : " + id);
             return HttpStatus.OK;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/ListingController.removeListing| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
--- src/main/java/com/sep/carsharingbusiness/restControllers/VehicleController.java
+++ src/main/java/com/sep/carsharingbusiness/restControllers/VehicleController.java
@@ -2,6 +2,7 @@ package com.sep.carsharingbusiness.restControllers;
 
 import com.google.gson.Gson;
 import com.sep.carsharingbusiness.graphQLServices.IVehicleService;
+import com.sep.carsharingbusiness.log.Log;
 import com.sep.carsharingbusiness.model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,7 +18,7 @@ public class VehicleController {
     private final Gson gson;
 
     @Autowired
-    public VehicleController(IVehicleService vehicleService) {
+    public VehicleController(IVehicleService vehicleService, Log log) {
         this.vehicleService = vehicleService;
         gson = new Gson();
     }
@@ -26,10 +27,14 @@ public class VehicleController {
     @GetMapping(value = "/vehicles")
     public synchronized String getVehicle(@RequestParam(value = "licenseNo") String licenseNo) {
         try {
-            return gson.toJson( vehicleService.getVehicle(licenseNo) );
+            Vehicle vehicle = vehicleService.getVehicle(licenseNo);
+            String result = gson.toJson(vehicle);
+            Log.addLog("|restControllers/VehicleController.getVehicle| : Request : " + licenseNo);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/VehicleController.getVehicle| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -38,10 +43,13 @@ public class VehicleController {
     public synchronized String addVehicle(@RequestBody String json) {
         try {
             Vehicle vehicle = gson.fromJson(json, Vehicle.class);
-            return gson.toJson( vehicleService.addVehicle(vehicle) );
+            String result = gson.toJson(vehicleService.addVehicle(vehicle));
+            Log.addLog("|restControllers/VehicleController.addVehicle| : Request : " + json);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/VehicleController.addVehicle| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -52,10 +60,13 @@ public class VehicleController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The licenceNo from param does not match with the vehicle licenseNo.");
         }
         try {
-            return gson.toJson( vehicleService.updateVehicle(vehicle) );
+            String result = gson.toJson(vehicleService.updateVehicle(vehicle));
+            Log.addLog("|restControllers/VehicleController.updateVehicle| : Reply :  " + result);
+            return result;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/VehicleController.updateVehicle| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
@@ -64,10 +75,12 @@ public class VehicleController {
     public synchronized HttpStatus removeVehicle(@RequestParam(value = "licenseNo") String licenseNo) {
         try {
             vehicleService.removeVehicle(licenseNo);
+            Log.addLog("|restControllers/VehicleController.removeVehicle| : Request : " + licenseNo);
             return HttpStatus.OK;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            Log.addLog("|restControllers/VehicleController.removeVehicle| : Error : " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
     }
