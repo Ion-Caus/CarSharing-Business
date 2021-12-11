f4e578f2c1dde81824d314618013cb1462857823
Ion-Caus <causion01@gmail.com>
2021-12-11 21:32:13 +0100

    added the IPaymentLogic "placeholder"

--- src/main/java/com/sep/carsharingbusiness/restControllers/MobilePayController.java
+++ src/main/java/com/sep/carsharingbusiness/restControllers/MobilePayController.java
@@ -3,6 +3,8 @@ package com.sep.carsharingbusiness.restControllers;
 
 import com.google.gson.Gson;
 import com.sep.carsharingbusiness.log.Log;
+import com.sep.carsharingbusiness.logic.IPaymentLogic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,30 +15,36 @@ import java.io.IOException;
 
 @RestController
 public class MobilePayController {
+    private final IPaymentLogic paymentLogic;
 
-    private final Gson gson;
-    private final String paymentAdress;
-
-    public MobilePayController(Gson gson) {
-        this.gson = gson;
-        this.paymentAdress = "123456789";
+    @Autowired
+    public MobilePayController(IPaymentLogic paymentLogic) {
+        this.paymentLogic = paymentLogic;
     }
 
-    //in the future here will be a connection to mobilepay API, from which the payment address will be obtained and
-    //also the final price will be sent to mobile pay to verify that the received amount is the correct amount
-
+    // TODO: 11.12.2021 by Ion - Send a payment obj with totalPrice, sender and receiver & change to Post
     @GetMapping(value = "/mobilepay")
-    public synchronized String getNewPaymentID() {
-
-            Log.addLog("|restControllers/MobilePayController.getNewPaymentID|");
-            return paymentAdress;
+    public synchronized String getPaymentID( @RequestParam(value = "amount") double amount) {
+        try {
+            Log.addLog("|restControllers/MobilePayController.getPaymentID| : Request : TotalPayment: " + amount + " dkk");
+            return paymentLogic.getPaymentID(amount);
+        }
+        catch (Exception e) {
+            Log.addLog("|restControllers/ListingController.getListingsByVehicle| : Error : " + e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
+        }
     }
 
     @GetMapping(value = "/mobilepay/verify")
-    public synchronized boolean verifyPayment() throws InterruptedException {
-        Log.addLog("|restControllers/MobilePayController.verifyPayment|");
-        Thread.sleep(3000);
-        return true;
+    public synchronized boolean verifyPayment(String paymentId) {
+        try {
+            Log.addLog("|restControllers/MobilePayController.verifyPayment|");
+            return paymentLogic.verifyPayment(paymentId);
+        }
+        catch (InterruptedException e) {
+            Log.addLog("|restControllers/ListingController.getListingsByVehicle| : Error : " + e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
+        }
     }
 
 }
